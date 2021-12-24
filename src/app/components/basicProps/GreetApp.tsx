8d6e40c3cf6c87f76d3dc17e31ad17022900e47b
Sasindu1997 <sasinduusgalhewa@gmail.com>
2021-12-24 12:36:26 +0530

    basic props using typescript

---
+++ src/app/components/basicProps/GreetApp.tsx
@@ -0,0 +1,11 @@
+import { Greet } from './Greet';
+
+function GreetApp() {
+    return (
+        <div className="GreetApp">
+            <Greet name="John" />
+        </div>
+    )
+}
+
+export default GreetApp
